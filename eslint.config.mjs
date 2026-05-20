a935f7c95e168a90d529419af1a1f8e0628f3067
Carven Martis <andante-ray.0s@icloud.com>
2026-05-20 18:59:05 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,15 @@ import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 const eslintConfig = [
   ...nextCoreWebVitals,
   {
+    rules: {
+      "jsx-a11y/aria-props": "error",
+      "jsx-a11y/aria-proptypes": "error",
+      "jsx-a11y/aria-unsupported-elements": "error",
+      "jsx-a11y/role-has-required-aria-props": "error",
+      "jsx-a11y/role-supports-aria-props": "error",
+    },
+  },
+  {
     ignores: [".next/**", "node_modules/**", "public/**"],
   },
 ];
